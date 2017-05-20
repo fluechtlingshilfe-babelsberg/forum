c3e3f0414cc10cd48fd3f6fae12cb200dc2fc4b3
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-05-20 11:27:13 +0200

    Basic ticket request functions

--- wp-content/themes/flueba-forum/functions.php
+++ wp-content/themes/flueba-forum/functions.php
@@ -10,9 +10,9 @@ require_once('lib/bootstrap-nav-walker.php');
 define('AVATAR_SIZE', 64);
 
 add_action('wp_enqueue_scripts', function() {
-    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
-    wp_enqueue_style('fontawesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css');
-    wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
+  wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+  wp_enqueue_style('fontawesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css');
+  wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
 
     wp_enqueue_script('bootstrap', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jquery'));
     wp_enqueue_script('flueba-comment', get_stylesheet_directory_uri() . '/js/comment.js', array('bootstrap'));
@@ -21,7 +21,7 @@ add_action('wp_enqueue_scripts', function() {
 add_theme_support('post-thumbnails');
 
 register_nav_menus(array(
-    'primary' => 'Primary Menu'
+  'primary' => 'Primary Menu'
 ));
 
 add_action('after_setup_theme', function() {
@@ -45,6 +45,28 @@ add_action('login_form', function() {
 add_filter('login_headerurl', function() {
     return get_option('siteurl');
 });
+add_action( 'admin_post_request_tickets', 'flueba_request_tickets' );
+function flueba_request_tickets() {
+  status_header(200);
+  $current_user = wp_get_current_user();
+  $event = get_post($_REQUEST['event']);
+  $num_tickets = get_field( "num_tickets", $_REQUEST['event'] );
+  $num_tickets_assigned = get_field( "num_tickets_assigned", $_REQUEST['event']);
+
+  $to      = 'flueba@mailinator.com';
+  $subject = 'Ticketanfrage';
+  $message = $current_user->display_name . ' hat ' .
+    $_REQUEST['number_of_tickets'] . ' Tickets für das Event ' . $event->post_title . " angefragt. " .
+    'Es sind noch ' . ($num_tickets - $num_tickets_assigned) . ' Tickets verfügbar.';
+  $headers = 'From: webmaster@example.com' . "\r\n" .
+  'Reply-To: ' . $current_user->user_email . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+  echo $message;
+  //mail($to, $subject, $message, $headers);
+  die();
+  //request handlers should die() when they complete their task
+}
+
 
 add_action('template_redirect', function() {
     if (flueba_on_public_page())
--- wp-content/themes/flueba-forum/single-kultuer_event.php
+++ wp-content/themes/flueba-forum/single-kultuer_event.php
@@ -3,36 +3,58 @@
 <?php the_post() ?>
 
 <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="<?= get_permalink(get_page_by_path('kultuer-veranstaltungen')) ?>">Veranstaltungen</a></li>
-    <li class="breadcrumb-item active"><?php the_title() ?></li>
+  <li class="breadcrumb-item"><a href="<?= get_permalink(get_page_by_path('kultuer-veranstaltungen')) ?>">Veranstaltungen</a></li>
+  <li class="breadcrumb-item active"><?php the_title() ?></li>
 </ol>
 
-<?php the_post_thumbnail('post-thumbnail', array('style' => 'width: auto; height: 300px', 'class' => 'float-sm-right')) ?>
+<div class="post-about clearfix mb-1">
+  <?php the_post_thumbnail('post-thumbnail', array('style' => 'width: auto; height: 300px', 'class' => 'float-sm-right')) ?>
 
-<h2><?php the_title() ?></h2>
-<p class="text-muted">
+  <h2><?php the_title() ?></h2>
+  <p class="text-muted">
     <span class="fa fa-fw fa-map-marker"></span>
     <?php the_field('place') ?>
     <br>
     <span class="fa fa-fw fa-clock-o"></span>
     <?= date_i18n(get_option('date_format'), strtotime(get_field('date'))) ?>
     <?php the_field('time') ?> Uhr
-</p>
-<?php the_content() ?>
+    <br>
+    <span class="fa fa-fw fa-ticket"></span>
+    <?php if (!get_field('no_booking')) { ?>
+      Noch <?php the_field('num_tickets') ?> Tickets verfügbar
+      <?php } else { ?>
+        Keine Anmeldung nötig
+        <?php } ?>
 
-<div class="btn-group-vertical">
-<a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-    <span class="fa fa-ticket fa-fw"></span>
-    Zwei Karten buchen
-</a>
-<a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-    <span class="fa fa-question fa-fw"></span>
-    Mehr Karten anfragen
-</a>
-<a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-    <span class="fa fa-info fa-fw"></span>
-    Zur Seite des Veranstalters
-</a>
-</div>
 
-<?php get_footer() ?>
+      </p>
+      <?php the_content() ?>
+    </div>
+
+    <div class="card card-block">
+      <form class="form-inline" action="<?= admin_url("admin-post.php") ?>" method="post">
+        <input type="hidden" name="action" value="request_tickets">
+        <input type="hidden" name="event" value="<?= get_the_ID() ?>">
+        <label class="sr-only" for="numberOfTickets">Name</label>
+        <input type="number" value="2" class="form-control mr-sm-2 mb-sm-0" name="number_of_tickets" id="numberOfTickets" placeholder="Anzahl" style="width: 4rem;">
+        Karten anfragen
+        <button type="submit" value="Submit" class="btn btn-primary">Anfragen</button>
+      </form>
+    </div>
+
+    <div class="btn-group-vertical">
+      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
+        <span class="fa fa-ticket fa-fw"></span>
+        Zwei Karten buchen
+      </a>
+      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
+        <span class="fa fa-question fa-fw"></span>
+        Mehr Karten anfragen
+      </a>
+      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
+        <span class="fa fa-info fa-fw"></span>
+        Zur Seite des Veranstalters
+      </a>
+    </div>
+
+    <?php get_footer() ?>
