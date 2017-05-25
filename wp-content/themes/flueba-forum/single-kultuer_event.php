3e45f84f54d603f16f3c344f85e9ed34885a9761
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-05-25 17:01:37 +0200

    Improve event page
    Still needs padding

--- wp-content/themes/flueba-forum/single-kultuer_event.php
+++ wp-content/themes/flueba-forum/single-kultuer_event.php
@@ -21,7 +21,8 @@
     <br>
     <span class="fa fa-fw fa-ticket"></span>
     <?php if (!get_field('no_booking')) { ?>
-      Noch <?php the_field('num_tickets') ?> Tickets verfügbar
+      Noch <?= ((int) get_field('num_tickets') - ((int) get_field('num_tickets_assigned'))) ?>
+       Tickets verfügbar
       <?php } else { ?>
         Keine Anmeldung nötig
         <?php } ?>
@@ -29,32 +30,36 @@
 
       </p>
       <?php the_content() ?>
-    </div>
+      <?php if (get_field('link')) { ?>
+        <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
+          <span class="fa fa-info fa-fw"></span>
+          Zur Seite des Veranstalters
+        </a>
+      <?php } ?>
 
-    <div class="card card-block">
-      <form class="form-inline" action="<?= admin_url("admin-post.php") ?>" method="post">
-        <input type="hidden" name="action" value="request_tickets">
-        <input type="hidden" name="event" value="<?= get_the_ID() ?>">
-        <label class="sr-only" for="numberOfTickets">Name</label>
-        <input type="number" value="2" class="form-control mr-sm-2 mb-sm-0" name="number_of_tickets" id="numberOfTickets" placeholder="Anzahl" style="width: 4rem;">
-        Karten anfragen
-        <button type="submit" value="Submit" class="btn btn-primary">Anfragen</button>
-      </form>
     </div>
-
-    <div class="btn-group-vertical">
-      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-        <span class="fa fa-ticket fa-fw"></span>
-        Zwei Karten buchen
-      </a>
-      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-        <span class="fa fa-question fa-fw"></span>
-        Mehr Karten anfragen
-      </a>
-      <a class="btn btn-secondary text-xs-left" href="<?php the_field('link') ?>" class="card-link">
-        <span class="fa fa-info fa-fw"></span>
-        Zur Seite des Veranstalters
-      </a>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="card card-block col-sm-12 col-md-5 col-lg-4">
+          <form action="<?= admin_url("admin-post.php") ?>" method="post">
+            <h3><small>Tickets anfragen</small></h3>
+            <input type="hidden" name="action" value="request_tickets">
+            <input type="hidden" name="event" value="<?= get_the_ID() ?>">
+            <div class="form-group">
+              <label class="sr-only" for="numberOfTickets">Anzahl der Tickets</label>
+              <div class="input-group">
+                <input type="number" value="2" class="form-control" name="number_of_tickets" id="numberOfTickets" placeholder="Anzahl">
+                <span class="input-group-addon">Karten anfragen</span>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="requestText">Anfragetext</label>
+              <textarea rows="3" class="form-control" id="requestText" name="request_text" placeholder="Besonders bei größeren Anfragen lohnt es sich, kurz zu beschreiben, wozu man die Tickets braucht."></textarea>
+            </div>
+            <button type="submit" value="Submit" class="btn btn-primary">Anfrage senden</button>
+          </form>
+        </div>
+      </div>
     </div>
 
     <?php get_footer() ?>
