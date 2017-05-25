316accd438ed964c87b8f62a9b529375d2e93ec0
Carl Gödecken <carl.goedecken@student.hpi.de>
2017-05-25 16:08:05 +0200

    Implement confirmation emails, check availablility

--- wp-content/themes/flueba-forum/admin-kultuer-accept-reject.php
+++ wp-content/themes/flueba-forum/admin-kultuer-accept-reject.php
@@ -1,13 +1,43 @@
 <?php
-define('DEFAULT_ACCEPT_TEXT', "Lieber Nutzer,\n\nIhr Ticket wurde bestätigt!");
-define('DEFAULT_REJECT_TEXT', "Lieber Nutzer,\n\nIhre Ticketanfrage wurde leider abgelehnt!");
+  define('DEFAULT_ACCEPT_TEXT', "Lieber Nutzer,\n\nIhr Ticket wurde bestätigt!");
+  define('DEFAULT_REJECT_TEXT', "Lieber Nutzer,\n\nIhre Ticketanfrage wurde leider abgelehnt!");
+  $user_id = $_GET['user_id'];
+  $user = get_user_by('ID', $user_id);
+  $num_tickets_requested = $_GET['num_tickets_requested'];
+  $event = get_post($_GET['event_id']);
+
+  $num_tickets_assigned = (int) get_field('num_tickets_assigned', $event->ID);
+  $num_tickets_total = get_field('num_tickets', $event->ID);
+  $num_tickets_available = $num_tickets_total - $num_tickets_assigned;
+  $has_tickets_left = $num_tickets_available > $num_tickets_requested;
 ?>
-<h1>Ticketanfrage <?= $_GET['ticketAction'] == 'accept' ? 'annehmen' : 'ablehnen' ?></h1>
+<h1>Ticketanfrage bearbeiten</h1>
+<p>
+  <b><?= $user->display_name ?></b> hat <?= $num_tickets_requested ?> Tickets für die Veranstaltung <a href="<?= get_permalink($event) ?>"><?= get_the_title($event) ?></a> angefragt.
+</p>
+Noch verfügbare Tickets: <b><?= $num_tickets_available ?></b> von insgesamt <b><?= $num_tickets_total ?></b>.
 <hr>
-<form action="confirm_tickets" method="post">
-  <textarea rows="8" class="regular-text" id="responseText"><?= $_GET['ticketAction'] == 'accept' ? DEFAULT_ACCEPT_TEXT : DEFAULT_REJECT_TEXT ?></textarea>
+<h2>Bestätigen</h2>
+<form action="admin-post.php" method="post">
+  <fieldset <?= $has_tickets_left ? '' : 'disabled="disabled"' ?>>
+    <textarea rows="8" class="regular-text" name="response_text"><?= DEFAULT_ACCEPT_TEXT ?></textarea>
+    <br>
+    <button class="button button-primary" value="submit">Bestätigung senden</button>
+    <input type="hidden" name="action" value="confirm_tickets">
+    <input type="hidden" name="event_id" value="<?= $event->ID ?>"></input>
+    <input type="hidden" name="num_tickets_requested" value="<?= $num_tickets_requested ?>"></input>
+    <input type="hidden" name="user_id" value="<?= $user_id ?>"></input>
+  </fieldset>
+</form>
+<?= $has_tickets_left ? '' : ('<a class="button button-primary" href="' . get_edit_post_link($event->ID) . '">Mehr Tickets freischalten</a>') ?>
+<hr>
+<h2>Ablehnen</h2>
+<form action="admin-post.php" method="post">
+  <textarea rows="8" class="regular-text" name="response_text"><?= DEFAULT_REJECT_TEXT ?></textarea>
   <br>
-  <button class="button button-primary" value="submit">Antwort senden</button>
-  <input type="hidden" id="event_id" value="<?= $_GET['eventId'] ?>"></input>
-  <input type="hidden" id="num_tickets_requested" value="<?= $_GET['numTicketsRequested'] ?>"></input>
+  <button class="button button-primary" value="submit">Ablehnung senden</button>
+  <input type="hidden" name="action" value="reject_tickets">
+  <input type="hidden" name="event_id" value="<?= $event->ID ?>"></input>
+  <input type="hidden" name="num_tickets_requested" value="<?= $num_tickets_requested ?>"></input>
+  <input type="hidden" name="user_id" value="<?= $user_id ?>"></input>
 </form>
--- wp-content/themes/flueba-forum/functions.php
+++ wp-content/themes/flueba-forum/functions.php
@@ -52,30 +52,31 @@ function kultuer_request_tickets() {
   // status_header(200);
   $current_user = wp_get_current_user();
   $event = get_post($_REQUEST['event']);
-  $num_tickets = get_field( "num_tickets", $_REQUEST['event'] );
+  $num_tickets_total = get_field( "num_tickets", $_REQUEST['event'] );
   $num_tickets_assigned = get_field( "num_tickets_assigned", $_REQUEST['event']);
+  $num_tickets_available = $num_tickets_total - $num_tickets_assigned;
+  $num_tickets_requested = $_REQUEST['number_of_tickets'];
 
   $response_parameters = array('page'=>'kultuer-accept-reject',
-                'num_tickets_requested'=>$num_tickets,
-                'event_id'=>$event->id);
-  $acceptAction = admin_url('options.php?ticketAction=accept&' . http_build_query($response_parameters));
-  $rejectAction = admin_url('options.php?ticketAction=reject&' . http_build_query($response_parameters));
+                'num_tickets_requested'=>$num_tickets_requested,
+                'event_id'=>$event->ID,
+                'user_id'=>$current_user->ID);
+  $answer_request_action = admin_url('options.php?' . http_build_query($response_parameters));
 
   $to      = 'flueba@mailinator.com';
   $subject = 'Ticketanfrage';
   $message = 'Der Nutzer ' . $current_user->display_name . ' hat ' .
-    $_REQUEST['number_of_tickets'] . ' Tickets für das Event "' . $event->post_title . '" angefragt. ' .
-    'Es sind noch ' . ($num_tickets - $num_tickets_assigned) . ' Tickets verfügbar.' . "<br>" .
-    '<a href="' . $acceptAction . '">Akzeptieren</a> ' .
-    '<a href="' . $rejectAction . '">Ablehnen</a>';
+    $num_tickets_requested . ' Tickets für das Event "' . $event->post_title . '" angefragt. ' .
+    'Es sind noch ' . $num_tickets_available . ' Tickets verfügbar.' . "<br>" .
+    '<a href="' . $answer_request_action . '">Antwort senden</a> ';
   $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: ' . $current_user->user_email . "\r\n" .
     'MIME-Version: 1.0' . "\r\n" .
     'Content-type: text/html; charset=UTF-8' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-  // echo $message;
-  mail($to, $subject, $message, $headers);
-  wp_redirect(get_permalink(get_page_by_path('kultuer-veranstaltungen')->ID) . '?message=Die%20Ticketanfrage%20wurde%20erfolgreich%20versendet.');
+   echo $message;
+  //mail($to, $subject, $message, $headers);
+  //wp_redirect(get_permalink(get_page_by_path('kultuer-veranstaltungen')->ID) . '?message=Die%20Ticketanfrage%20wurde%20erfolgreich%20versendet.');
   die();  //request handlers should die() when they complete their task
 }
 
@@ -87,8 +88,39 @@ add_action('admin_menu', function() {
 
 add_action( 'admin_post_confirm_tickets', 'kultuer_confirm_tickets' );
 function kultuer_confirm_tickets() {
-
+  $user = get_user_by('ID', $_REQUEST['user_id']);
+  $num_tickets_requested = $_REQUEST['num_tickets_requested'];
+  $event = get_post($_REQUEST['event_id']);
+  $subject = 'Ihre Ticketanfrage für die Veranstaltung ' . get_the_title($event) . ' wurde bestätigt';
+  kultuer_send_ticket_response_mail($user->user_email, $subject, $_REQUEST['response_text']);
+  kultuer_save_ticket_assignment($event, $num_tickets_requested);
   die();  //request handlers should die() when they complete their task
+}
+
+function kultuer_save_ticket_assignment($event, $num_tickets_granted) {
+  $num_tickets_assigned = (int) get_field('num_tickets_assigned', $event->ID);
+  $num_tickets_assigned += $num_tickets_granted;
+  update_field('num_tickets_assigned', $num_tickets_assigned, $event->ID);
+}
+
+add_action( 'admin_post_reject_tickets', 'kultuer_reject_tickets' );
+function kultuer_reject_tickets() {
+  $user = get_user_by('ID', $_REQUEST['user_id']);
+  $event = get_post($_REQUEST['event_id']);
+  $subject = 'Ihre Ticketanfrage für die Veranstaltung ' . get_the_title($event) . ' wurde abgelehnt';
+  kultuer_send_ticket_response_mail($user->user_email, $subject, $_REQUEST['response_text']);
+  die();  //request handlers should die() when they complete their task
+}
+
+function kultuer_send_ticket_response_mail($_to, $subject, $message) {
+  $to      = 'fluebauser@mailinator.com'; // TODO: remove
+  $headers = 'From: webmaster@example.com' . "\r\n" . // TODO
+    'Reply-To: ' . wp_get_current_user()->user_email . "\r\n" .
+    'MIME-Version: 1.0' . "\r\n" .
+    'Content-type: text/html; charset=UTF-8' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+   echo $message;
+  // mail($to, $subject, $message, $headers);
 }
 
 add_action('template_redirect', function() {
