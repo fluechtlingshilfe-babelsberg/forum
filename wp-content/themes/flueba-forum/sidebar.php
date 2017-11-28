6696c547b909b6837a9fe5984ce3d0b496253fc1
Tom Beckmann <tomjonabc@gmail.com>
2017-11-28 11:13:56 +0100

    Hide placeholder, outdated events (todo: implement events)

--- wp-content/themes/flueba-forum/sidebar.php
+++ wp-content/themes/flueba-forum/sidebar.php
@@ -47,13 +47,14 @@ wp_reset_postdata(); ?>
     <span class="fa fa-calendar"></span>
     Veranstaltungen
 
-    <a target="_blank" class="category-list-item" href="<?= get_stylesheet_directory_uri() . '/downloads/Einladung zum Sommerfest AWO am Brauhausberg.pdf' ?>">
+    <p>Wird demnächst aktualisiert!</p>
+    <!--<a target="_blank" class="category-list-item" href="<?= get_stylesheet_directory_uri() . '/downloads/Einladung zum Sommerfest AWO am Brauhausberg.pdf' ?>">
 	AWO Sommerfest <small class="text-muted">Do, 13. Juli</small>
     </a>
 
     <a target="_blank" class="category-list-item" href="<?= get_stylesheet_directory_uri() . '/downloads/Supervision_Richtig ankommen_10.07.2017.pdf' ?>">
 	Supervision <small class="text-muted">Mo, 10. Juli</small>
-    </a>
+    </a>-->
 </div>
 
 <?php if (is_singular('post'))
